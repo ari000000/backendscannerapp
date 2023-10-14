8f34169910c9db0ca7c893c12f90f3504e13a0ad
ari000000 <playstation369@outlook.com>
2023-10-14 12:21:09 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const app = express();
+const PORT = 4000;
+
+// Middleware, um JSON-Requests zu verarbeiten
+app.use(bodyParser.json());
+
+// Route, um alle Produkte zu holen
+app.get('/produkte', (req, res) => {
+    fs.readFile('produkte.json', 'utf-8', (err, data) => {
+        if (err) {
+            return res.status(500).send('Serverfehler beim Lesen der Datei.');
+        }
+        const produkte = JSON.parse(data || '[]');
+        res.json(produkte);
+    });
+});
+
+// Route, um ein neues Produkt hinzuzufügen
+app.post('/produkte', (req, res) => {
+    const neuesProdukt = req.body;
+
+    fs.readFile('produkte.json', 'utf-8', (err, data) => {
+        if (err) {
+            return res.status(500).send('Serverfehler beim Lesen der Datei.');
+        }
+
+        const produkte = JSON.parse(data || '[]');
+        produkte.push(neuesProdukt);
+
+        fs.writeFile('produkte.json', JSON.stringify(produkte, null, 2), (err) => {
+            if (err) {
+                return res.status(500).send('Serverfehler beim Speichern der Datei.');
+            }
+            res.status(201).send(neuesProdukt);
+        });
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server läuft auf Port ${PORT}`);
+});
