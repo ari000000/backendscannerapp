0b6bf66945f3f7671b225083cf80e89bd7c7ab95
ari000000 <playstation369@outlook.com>
2023-10-14 14:22:47 +0200

    fix 16

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 const app = express();
-const PORT = 4000;
+
 
 
 const cors = require('cors');
